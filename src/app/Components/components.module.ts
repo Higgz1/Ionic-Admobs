902238954cf02f358c5a911363a0ce6016906cea
Bryan Higgz <bryan.kahiga@gmail.com>
2022-06-03 12:05:01 +0300

    Added new components

--- src/app/Components/components.module.ts
+++ src/app/Components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { BannerComponent } from './banner/banner.component';
 import { InterstitialComponent } from './interstitial/interstitial.component';
+import { RewardVideoComponent } from './reward-video/reward-video.component';
 
 @NgModule({
-  declarations: [BannerComponent, InterstitialComponent],
+  declarations: [BannerComponent, InterstitialComponent, RewardVideoComponent],
   imports: [CommonModule, IonicModule],
-  exports: [BannerComponent, InterstitialComponent],
+  exports: [BannerComponent, InterstitialComponent, RewardVideoComponent],
 })
 export class ComponentsModule {}
